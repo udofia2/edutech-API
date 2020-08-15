00809eea85b46ce0c41d20175c7fd804cf7aa587
udofia2 <enalsy22@gmail.com>
2020-08-15 01:48:30 +0100

    Student registration implemented

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const mongoose = require('mongoose')
 const config = require('./config/default')
 require('./config/db')(mongoose)
 
-
 const app = express()
 
 app.use(bodyParser.json())
 app.use(express.urlencoded({extended: true}))
+
+app.use('/', require('./routes/studentRegistration.Router'))
 
 app.listen(config.PORT, () => console.log(`Server is running on localhost:${config.PORT}`))
